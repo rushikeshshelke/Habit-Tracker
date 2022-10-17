49c651574458cb8b7d12715304eeeb915b36bcae
rushikeshshelke <shelke.rushi2010@gmail.com>
2022-10-17 21:36:17 +0530

    app.py bug fix

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def createApp():
     app.db = client.get_database(os.environ.get("DATABASE_NAME"))
     app.run(host="0.0.0.0",port=os.environ.get("PORT"),debug=True)
 
-if __name__ == "__main__":
-    createApp()
+# if __name__ == "__main__":
+#     createApp()
